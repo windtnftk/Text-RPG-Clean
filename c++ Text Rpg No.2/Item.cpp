36fb75fddda111fd574e0ace004f761ed22f4dba
chanhwi Han <windtnftk@naver.com>
2024-12-08 20:50:52 +0900

    장비창 유지 및 아이템 가방 재 조정작업 완료 함수 대량 삭제 및 최적화, 아이템 도감과 메인 아이템 함수의 역활 분리 완료 public 과 private 분리작업 진행예정 그 후 문제 없으면 상점 함수 제작 예정, pch.h에 다 넣을필요 없는데 넣은것들 분리 예정

--- c++ Text Rpg No.2/Item.cpp
+++ c++ Text Rpg No.2/Item.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <unordered_set>
 //#include "Item.h"
 //#include "Ccore.h"
 
@@ -39,13 +40,13 @@ void MainItem::ItemInit()
 {		/*
 		//item을 랜덤으로 3개만 추가 하고 싶은데
 		// 일단 5,2,6 이런식으로 고정된 시드값 넣어보자
-		
+
 		for (int i = 0; i < (int)ItemId::End; ++i)
 		{
 			NewItemInfo NewItem = { static_cast<ItemId>(i),ItemArr[i]};
 			ItemBag.push_back(NewItem);
 		}
-		
+
 		std::random_device rd;
 		std::mt19937 gen(rd()); // Mersenne Twister 알고리즘을 사용하는 엔진 생성
 		// 균일 분포에서 랜덤한 정수 생성 (0 이상 99 이하)
@@ -57,30 +58,31 @@ void MainItem::ItemInit()
 			ItemBag.push_back(NewItem);
 		}
 		*/
-	
+
 	AddItem(ItemId::Weapon1);
 	AddItem(ItemId::Equipment1);
-	AddItem(ItemId::HealthPortion);
-	handleItem = ItemBag.begin();
+	AddItem(ItemId::HealthPortion, 3);
+	//handleItem = ItemBag.begin();
 	OpenItemBag();
 }
-void MainItem::HandleItemErase(vector<NewItemInfo>::iterator& ItemId)
-{
-	if (ItemId != ItemBag.end())
-	{
-		ItemBag.erase(ItemId);
-		handleItem = ItemBag.begin();
-	}
-}
+
+//void MainItem::HandleItemErase(vector<NewItemInfo>::iterator& ItemId)
+//{
+//	if (ItemId != ItemBag.end())
+//	{
+//		ItemBag.erase(ItemId);
+//		handleItem = ItemBag.begin();
+//	}
+//}
 void MainItem::OpenItemBag()
 {
-	auto it = ItemBag.begin();
-	for (int i = 0; it != ItemBag.end(); ++it,++i)
+	int count = 0;
+	for (auto q : ItemBag)
 	{
-		string GetName = SelectName(it);
-		std::cout << i + 1 << ". " << GetName << std::endl;
+		std::cout << count++ << ". " << ItemName(q.first) << ", 개수: " << q.second << std::endl;
 	}
 }
+
 //ItemId MainItem::SelectId(const vector<NewItemInfo>::iterator& ItemId)
 //{
 //	if (ItemId == ItemBag.end())
@@ -93,29 +95,90 @@ void MainItem::OpenItemBag()
 //		return ItemId->Name;
 //	}}
 
-string MainItem::SelectName(const vector<NewItemInfo>::iterator& ItemId)
-{
-	NewItemInfo Hi = *ItemId;
-	return (string)Hi.Name;
-}
+
 void MainItem::UseItemManuOpen()
 {
 	OpenItemBag();
 	// 아이템 을 고르고 사용을 할지 정하자
 	std::cout << std::endl << "사용하실 아이템을 고르세요" << std::endl;
-	int choice = CinAuto();
-	if (0 == choice || choice > ItemBag.size() || std::cin.fail())
-	{
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		ErrorCode();
-		return;
-	}
-	ItemTypeEffect(convert(choice));
+	UseItemChange(ItemBagId_to_cinAuto());
 }
-void MainItem::AddItem(ItemId item)
+void MainItem::UseItemChange(ItemId id)
 {
-	ItemBag.emplace_back(ItemEncyclopedia::GetInst()->getNewItemInfo(item));
+	// 소모성 아이템일경우
+	if (ItemTypeCheck(id)) UseItem(id); 
+	// 장비 아이템일경우
+	else equippedUse(id);
+}
+void MainItem::equippedUse(ItemId id)
+{
+	// 장비만 사용하는 함수임
+	if (!ItemTypeCheck(id)) { std::cout << "장비가 아닙니다." << std::endl; return; }
+	// 장비 찾기
+	auto it = ItemBag.find(id);
+	// 해당 장비는 가지고 있지 않음
+	if (it == ItemBag.end()) { std::cout << "아이템창에 없는 장비 입니다." << std::endl;  return; }
+	// 개수가 0개 이하 임
+	if (it->second <= 0) { std::cout << "장비 개수가 0개 이하입니다." << std::endl; return; }
+	// 장비 추가 성공시에만 아이템백에서 아이템 제거 및 효과적용
+	if (!AddequippedItems(id)) { std::cout << "장비창에 같은 장비가 있습니다." << std::endl; return; }
+	// 아이템 숫자 줄이고 아이템 사용효과 빌동, 아이템 개수 적으면 아예 삭제
+	--it->second; ItemEffect(id); if (it->second <= 0)
+	{
+		ItemBag.erase(it); std::cout << "해당 장비를 다 사용했습니다." << std::endl;
+	}
+	std::cout << "장비 장착 완료." << std::endl;
+}
+void MainItem::UseItem(ItemId id)
+{
+	// 소모성 아이템만 사용하는 함수임
+	if (ItemTypeCheck(id)) { std::cout << "소모성 아이템이 아닙니다." << std::endl; return; }
+	// 아이템 찾기
+	auto it = ItemBag.find(id);
+	// 해당아이템은 가지고 있지 않음
+	if (it == ItemBag.end()) { std::cout << "아이템창에 없는 소모성아이템 입니다." << std::endl;  return; }
+	// 개수가 0개 이하 임
+	if (it->second <= 0) { std::cout << "아이템 개수가 0개 이하입니다." << std::endl; return; }
+	// 아이템 숫자 줄이고 아이템 사용효과 빌동, 아이템 개수 적으면 아예 삭제
+	--it->second; ItemEffect(id); if (it->second <= 0)
+	{ ItemBag.erase(it); std::cout << "해당 아이템을 다 사용했습니다." << std::endl; }
+}
+ItemId MainItem::ItemBagId_to_cinAuto()
+{
+	auto it = ItemBag.begin();
+	int choice = CinAuto();
+	// std::advance로 반복자를 이동
+	return std::next(it, choice)->first;
+}
+ItemId MainItem::EquippedId_to_cinAuto()
+{
+	auto it = equippedItems.begin();
+	auto id = std::next(it, CinAuto()); // 범위 계산을 위한 이동
+	if (id == equippedItems.end()) return ItemId::End; // 잘못된 값 입력시 반환값 End
+	return *id;
+}
+bool MainItem::ItemTypeCheck(ItemId id)
+{
+	if (ItemId::End == id) { std::cout << "잘못된 Id값 입니다." << std::endl; return false; }
+	return ItemEncyclopedia::GetInst()->getNewItemInfo(id).Type == ItemType::CONSUMABLE;
+}
+void MainItem::AddItem(ItemId item) {
+	ItemBag[item]++;
+}
+void MainItem::AddItem(ItemId item, int NUMBER) {
+	ItemBag[item] += NUMBER;
+}
+bool MainItem::LikeItemtypeCheck(ItemId id)
+{
+	for (auto q : equippedItems)
+	{
+		if (Itemtype(q) == Itemtype(id))
+		{
+			std::cout << "같은 type의 장비가 있습니다." << std::endl;
+			return false;
+		}
+	}
+	return true;
 }
 void MainItem::TotalequippedItems()
 {
@@ -123,221 +186,267 @@ void MainItem::TotalequippedItems()
 	std::cout << "장비를 해체하시겠습니까?" << std::endl;
 	if (!Yes_No()) return;
 	std::cout << "어떤장비를 해체하시겠습니까?" << std::endl;
-	MoveEquipped(CinAuto());
-	
+	MoveEquipped(EquippedId_to_cinAuto());
+
+}
+bool MainItem::AddequippedItems(ItemId id)
+{
+	if (!LikeItemtypeCheck(id)) return false; // 같은 타입의 장비를 장착하려할경우 false
+	return equippedItems.insert(id).second;
 }
 bool MainItem::ViewEquippedItems()
 {
-	std::cout << "장착중인 장비" ;
+	std::cout << "장착중인 장비";
 	if (equippedItems.empty())
 	{
-		std::cout <<"가 없습니다."<< std::endl;
+		std::cout << "가 없습니다." << std::endl;
 		return false;
 	}
 	std::cout << std::endl;
+	int count = 0;
 	for (auto q : equippedItems)
 	{
-		int it = static_cast<int>(q);
-		std::cout << it + 1<<". " << std::endl << ItemArr[it] << ", " << std::endl;
+		std::cout << count++ << ". " << ItemName(q) << std::endl;
 	}
 	return true;
 }
-void MainItem::MoveEquipped(int choice)
-{	// TODO: Choice부분 1을 강제로 더하고 뺴는 부분을 수정해야됨, ViewEquippedItems포함
-	choice -= 1;
-	if (choice < 0|| choice >= equippedItems.size())
-	{
-		ErrorCode();
-		return;
-	}
-	auto it = equippedItems.begin() + choice;
-	equippedItems.erase(it); // 장비 해체
-	AddItem(convert(choice)); // 아이템으로 복귀
-}
-// 아이템 효과 초기화
-void MainItem::initEffects()
+void MainItem::MoveEquipped(ItemId id)
 {
-	
-}
-// 장비 장착 함수
-void MainItem::ItemTypeEffect(ItemId id) {
-	if (ItemId::Weapon1 > id)
-	{
-		applyEffect(id);
-	}
-	else if (std::find(equippedItems.begin(), equippedItems.end(), id) == equippedItems.end())
-	{	
-		// NowCoding: 24.11.30 아이템 도감 만들고, ItemBag을 만들면서 드는생각 ItemBag을 전체 정보를 받을 필요가 있나?
-		// 그냥 ItemId만 가지고 있고 효과 적용시에 도감 통해서 효과 적용하고 관리하고 ItemBag은 뭘 가지고 있는지만
-		// 체크하면 되는거 같은데 아이템 장비도 bool 변수로 그냥 사용여부 체크하고 ㅇㅇ 일단 여기 함수 고치면서 생각 ㄱ
-		// 장비 장착함수 및 변수 삭제 생각중 그냥 아이템이용한 동작을
-		// Main Item에서 하고 그냥 실제 효과 적용을 도감에서 하자
-		// 중복 장착 방지
-		//if()
-		equippedItems.push_back(id); 
-		applyEffect(id);             // 장비 효과 적용
-	}
-	else {
-		std::cout << "Item is already equipped.\n";
-	}
-}
-// 장비 해제 함수
-void MainItem::UnItemTypeEffect(ItemId id) {
-	auto it = std::find(equippedItems.begin(), equippedItems.end(), id);
-	if (it != equippedItems.end()) {
-		RemoveEffect(id);            // 장비 효과 제거
-		equippedItems.erase(it);         // 목록에서 제거
-	}
-	else {
-		std::cout << "Item is not equipped.\n";
-	}
-}
-// 장착된 모든 장비의 효과를 일괄 적용
-void MainItem::applyEquippedItems() {
-	for (const auto& item : equippedItems) {
-		applyEffect(item);
-	}
-}
-void MainItem::applyEffect(ItemId test)
-{
-	ItemEncyclopedia::GetInst()->getNewItemInfo(test).effects();
-}
-void MainItem::RemoveEffect(ItemId test)
-{
-	if (test < ItemId::Weapon1) 
-	{
-		std::cout << "사용 item은 탈착할수 없습니다." << std::endl;
-		return;
-	}
-	 // ItemId에 해당하는 효과 함수 검색
-	ItemEncyclopedia::GetInst()->getNewItemInfo(test).removeEffects();
-	//해당 함수 호출하여 아이템 효과 적용
+	// 장비 아이템만 사용하는 함수임
+	if (!ItemTypeCheck(id)) { std::cout << "소모성 아이템입니다." << std::endl; return; }
+	// 아이템 찾기
+	auto it = equippedItems.find(id);
+	// 해당아이템은 가지고 있지 않음
+	if (it == equippedItems.end()) { std::cout << "장비창에 없는 아이템입니다." << std::endl;  return; }
+	// 아이템 삭제효과 빌동, 아이템 개수 적으면 아예 삭제
+	RemoveItemEffect(id);
+	AddItem(id);  std::cout << "장비 탈착완료되었습니다." << std::endl;
 }
 // 아이템 효과 대충 만듬 나중에 밸런스 보면서 넣기
 // 아이템 사용
 #pragma region Potion Effects
 void ItemEncyclopedia::HealthPortionEffect()
-{ Ccore::GetInst()->LimitHealthUp(5); }
+{
+	Ccore::GetInst()->LimitHealthUp(5);
+}
 void ItemEncyclopedia::BigHealthPortionEffect()
-{ Ccore::GetInst()->LimitHealthUp(5); }
+{
+	Ccore::GetInst()->LimitHealthUp(5);
+}
 void ItemEncyclopedia::PowerPortionEffect()
-{ Ccore::GetInst()->LimitHealthUp(5); }
+{
+	Ccore::GetInst()->LimitHealthUp(5);
+}
 void ItemEncyclopedia::BigPowerPortionEffect()
-{ Ccore::GetInst()->LimitHealthUp(5); }
+{
+	Ccore::GetInst()->LimitHealthUp(5);
+}
 void ItemEncyclopedia::Potion4Effect()
-{ Ccore::GetInst()->LimitHealthUp(5); }
+{
+	Ccore::GetInst()->LimitHealthUp(5);
+}
 void ItemEncyclopedia::Potion5Effect()
-{ Ccore::GetInst()->Newbarrier(5); }
+{
+	Ccore::GetInst()->Newbarrier(5);
+}
 void ItemEncyclopedia::Potion6Effect()
-{ Ccore::GetInst()->Newbarrier(5); }
+{
+	Ccore::GetInst()->Newbarrier(5);
+}
 void ItemEncyclopedia::Potion7Effect()
-{ Ccore::GetInst()->Newbarrier(5); }
+{
+	Ccore::GetInst()->Newbarrier(5);
+}
 void ItemEncyclopedia::FirePortionEffect()
-{ Ccore::GetInst()->Newbarrier(5); }
+{
+	Ccore::GetInst()->Newbarrier(5);
+}
 void ItemEncyclopedia::BigFirePortionEffect()
-{ Ccore::GetInst()->Newbarrier(5); }
+{
+	Ccore::GetInst()->Newbarrier(5);
+}
 #pragma endregion
 // 무기 장착
 #pragma region Weapon Effects										
 void ItemEncyclopedia::Weapon1Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon2Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon3Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon4Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon5Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon6Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon7Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon8Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon9Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 void ItemEncyclopedia::Weapon10Effect()
-{ Ccore::GetInst()->AddPowerUp(5); }
+{
+	Ccore::GetInst()->AddPowerUp(5);
+}
 #pragma endregion
 // 장비 장착
 #pragma region Equipment Effects		
 void ItemEncyclopedia::Equipment1Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment2Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment3Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment4Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment5Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment6Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment7Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment8Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment9Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 void ItemEncyclopedia::Equipment10Effect()
-{ Ccore::GetInst()->AddDefenseUp(5); }
+{
+	Ccore::GetInst()->AddDefenseUp(5);
+}
 #pragma endregion
 // 무기 해체
 #pragma region Remove Weapon Effects		
 void ItemEncyclopedia::RemoveWeapon1Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon2Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon3Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon4Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon5Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon6Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon7Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon8Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon9Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 void ItemEncyclopedia::RemoveWeapon10Effect()
-{ Ccore::GetInst()->RemovePowerUp(5); }
+{
+	Ccore::GetInst()->RemovePowerUp(5);
+}
 #pragma endregion
 // 장비 해체
 #pragma region Remove Equipment Effects	
 void ItemEncyclopedia::RemoveEquipment1Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment2Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment3Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment4Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment5Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment6Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment7Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment8Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment9Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 void ItemEncyclopedia::RemoveEquipment10Effect()
-{ Ccore::GetInst()->RemoveDefenseUp(5); }
+{
+	Ccore::GetInst()->RemoveDefenseUp(5);
+}
 #pragma endregion
 
 std::function<void()> ItemEncyclopedia::getEffectFunction(ItemId id) {
 	switch (id) {
 	case ItemId::HealthPortion:		return [this]() { this->HealthPortionEffect();		};
 	case ItemId::BigHealthPortion:	return [this]() { this->BigHealthPortionEffect();	};
-	case ItemId::PowerPortion:		return [this]() { this->PowerPortionEffect() ;		};
-	case ItemId::BigPowerPortion:	return [this]() { this->BigPowerPortionEffect() ;	};
-	case ItemId::Potion4:			return [this]() { this->Potion4Effect() ;			};
-	case ItemId::Potion5:			return [this]() { this->Potion4Effect() ;			};
+	case ItemId::PowerPortion:		return [this]() { this->PowerPortionEffect();		};
+	case ItemId::BigPowerPortion:	return [this]() { this->BigPowerPortionEffect();	};
+	case ItemId::Potion4:			return [this]() { this->Potion4Effect();			};
+	case ItemId::Potion5:			return [this]() { this->Potion4Effect();			};
 	case ItemId::Potion6:			return [this]() { this->Potion4Effect();			};
 	case ItemId::Potion7:			return [this]() { this->Potion5Effect();			};
 	case ItemId::FirePortion:		return [this]() { this->FirePortionEffect();		};
@@ -396,11 +505,25 @@ ItemEncyclopedia::ItemEncyclopedia()
 {
 	for (int i = 0, k = 0; i < static_cast<int>(ItemId::End); ++i)
 	{
+		ItemType it;
 		k = (i % 10) / 2; // 아이템 레어도 맞추기
 		ItemId id = Id_Convert(i);
+		if (id < ItemId::Weapon1)
+		{
+			it = ItemType::CONSUMABLE;
+		}
+		else if (id < ItemId::Equipment1)
+		{
+			it = ItemType::WEAPON;
+		}
+		else
+		{
+			it = ItemType::EQUIPMENT;
+		}
 		itemData[id] = {
 			ItemArr[i],
 			Rarity_Convert(k),
+			it,
 			getEffectFunction(id),
 			getRemoveEffectFunction(id),false
 		};
@@ -412,10 +535,9 @@ ItemEncyclopedia::~ItemEncyclopedia()
 }
 MainItem::MainItem()
 	:ItemBag{}
-	, handleItem{}
 	, equippedItems{}
 {
-	
+
 }
 MainItem::~MainItem()
 {
--- c++ Text Rpg No.2/Item.h
+++ c++ Text Rpg No.2/Item.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <unordered_set>
 enum class ItemId
 {
 	HealthPortion,
@@ -34,6 +34,11 @@ enum class ItemId
 	Equipment10,
 	End = 30,
 };
+enum class ItemType {
+	CONSUMABLE,  // 소모성 아이템
+	WEAPON,      // 무기
+	EQUIPMENT    // 장비
+};
 enum ItemRarity {
 	COMMON,      // 일반 아이템 1~2 번
 	UNCOMMON,    // 고급 아이템 3~4 번
@@ -45,6 +50,7 @@ struct NewItemInfo
 {
 	string	Name; // 이름
 	ItemRarity Rarity; // 레어도
+	ItemType	Type;	// 아이템 타입
 	std::function<void()> effects;		// 착용시 효과
 	std::function<void()> removeEffects;// 탈착시 효과
 	bool E_Check = false;				// 장착되어 있는지 체크
@@ -130,50 +136,88 @@ private:
 	void RemoveEquipment10Effect();
 #pragma endregion
 };
-// 그러니까 아이템을 사용할때 이름값만 가져오면 어떤아이템을 사용되는지 
-// 확인되고 사용하는 함수? 를 만들고 싶은거자나
-// enum class 는 그렇게 사용하고 여기에는 결국 뭐를 만들고 싶은거냐 
-// 일단 매니저로 사용하는거는 나중에 분리하고 모든기능을 
-// 이 클래스에 때려놓고
-// 모듈화를 천천히 진행하자
-// 지금 아이템을 추가하는 함수는 우리가 진행하는 
-// 함수에서 리스트(변수가 아닌 ItemArr에서 가져옴)
-// 그러면 아이템 백은 그대로 쓰고 아이템 도감을 실제로 만들고
-// 거기에 있는 정보를 가져오는게 
-
+// NowCoding : public 과 private 분리작업 진행예정 그 후 문제 없으면 상점 함수 제작 예정
 class MainItem
 {
 	SINGLE(MainItem);
 public:
 	// TODO: 아직 아이템 장비 또는 사용효과 적용은 세세하게 구현 안했음, 추후에 해야됨
+	// ItemBag 에서 남길것들
+	// 1.아이템 초기화 : 초기 아이템 3개 정도 있고, handle Item은 nullrt로 만들어서 지정후 지울수 있도록
+	// 
+	// 5. 아이템 추가 함수 : 아이템 Id를 받아서 추가하는 함수
+	// 두가지 형태의 함수로 만들어서 int 인수 받을경우 그 수 만큼 추가, Id값만 받을 경우 1개만 추가
+	// 내부 구조는 ID 탐색 후 원래 있으면 개수만 추가 없을경우 새로 만들고
+	// 
+	// 다시 만들자 새로 ㅇㅇ
+	std::unordered_map<ItemId, int> ItemBag; // 아이템 가방
+	std::unordered_set<ItemId> equippedItems; // 장비중인 장비
 	// 아이템 init
-	void ItemInit();
+	void ItemInit(); // 완료
 	// 아이템 사용하는 함수
-	void UseItemManuOpen();
+	void UseItemManuOpen(); //완료
+private:
+	// 고른 아이템이 장착아이템일 경우와 아닐경우 나눠서 다른 함수 호출
+	void UseItemChange(ItemId id);//완료
+	//3. 장비 장착 함수 : 장비된 아이템을 관리 할 변수에 ItemId 넣고 아이템 효과 함수 출력
+	// 진행중 아이템 삭제도 진행
+	// 아이템 삭제를 진행할때 아이템 백에서 개수 1개를 내리고 그 후에 0개가 될경우 Id값도 없애
+	// 그 경우 handle Item을 재지정 하고 Erase 함수 출력(따로 만듬, 분활)
+	void equippedUse(ItemId id);//완료
+	// 2.소모성 아이템 사용함수 : 여기서는 아이템 Id를 받고 이게 소모성 아이템인지 확인하여 맞을경우 진행
+	// 소모성 아이템과 무기 아이템, 장비아이템을 아이템 도감에서 이 값을 받아오는 함수생성
+	// 여기서 아이템 무기, 장비 장착 함수도 작동하게 만들어야겠다
+	void UseItem(ItemId id); // 완료
+	// OpenItemBag에 맞추어 ItemId를 인수를 줄 함수
+	ItemId ItemBagId_to_cinAuto(); //완료
+	// equippedItems에 맞추어 ItemId를 인수를 줄 함수
+	ItemId EquippedId_to_cinAuto();//완료
+	// ItemId 확인해서 소모성 아이템인지 확인하는 함수
+	// 소모성 아이템일경우 true 출력
+	bool ItemTypeCheck(ItemId id);//완료
+	// ItemId 인수로 받으면 그에 맞는 아이템 효과 출력 //완료
+	void ItemEffect(ItemId id) { ItemEncyclopedia::GetInst()->getNewItemInfo(id).effects; }
+	// ItemId 인수로 받으면 그에 맞는 아이템 삭제효과 출력 //완료
+	void RemoveItemEffect(ItemId id) { ItemEncyclopedia::GetInst()->getNewItemInfo(id).removeEffects; }
+	// ItemId 인수로 받으면 이름 출력 //완료
+	string ItemName(ItemId id) { return ItemEncyclopedia::GetInst()->getNewItemInfo(id).Name; }
+	// ItemId 인수로 받으면 어떤종류의 아이템인지 알려줌
+	ItemType Itemtype(ItemId id) { return ItemEncyclopedia::GetInst()->getNewItemInfo(id).Type; }
+	// ItemId 인수로 받으면 지금 id가 장비창에 있는 아이템 type하고 곂치는지 확인
+	// 장비창 탐색중 같은 아이템이 장착되지 않은것이 확인되면 true, 곂치면 false
+	bool LikeItemtypeCheck(ItemId id); //완료
 	// 장비 해체 및 관리 총관리 함수
 	// 장착한장비 보여주고(없으면 장비없다고 바로반환)
 	// 장비해체 할건지 물어봄(여기서는 장비 해체만 가능)
-	void TotalequippedItems();
+	void TotalequippedItems(); // 완료
+	// ItemId 가 인수로 들어오면 장비창에 추가하는 함수
+	// 장비 추가 실패(같은 장비가 있음 등)시 false출력
+	bool AddequippedItems(ItemId id); // 완료
+	// 장비장착 함수
 private:
 	// 아이템의 실제 저장공간
-	vector<NewItemInfo>  ItemBag;
+	//vector<NewItemInfo>  ItemBag;
 	//손에 있는 아이템
-	vector<NewItemInfo>::iterator handleItem; //일단 privite 로 만들어서 사용해야 될듯
+	//vector<NewItemInfo>::iterator handleItem; //일단 privite 로 만들어서 사용해야 될듯
 private:
 	//HandleItem 지우기, 일단 privite 로 만들어서 사용해야 될듯
 	void HandleItemErase(vector<NewItemInfo>::iterator& ItemId);
 	// 아이템 찾아주는 함수 그리고 아이템 나열 하는 함수(public으로 바꿀지 고민)
-	void OpenItemBag();
+	// 4. ItemBag 확인 하는 함수 : 아이템 Id 및 개수까지만 보여주기만 함
+	
+	void OpenItemBag(); // 완료
 	// iterator 주소값을 id int값 뽑아오는 함수
 	//temId SelectId(const vector<NewItemInfo>::iterator& ItemId);
 	
 	// iterator 참조값을 id string값 뽑아오는 함수
-	string SelectName(const vector<NewItemInfo>::iterator& ItemId);
+	
 	
 	// ItemId 입력하면 장비에 추가하는 함수
-	void AddItem(ItemId item);
+	void AddItem(ItemId item); //완료
+	// ItemId 입력하면 장비에 추가하는 함수, int값 넣으면 그 수만큼 넣음
+	void AddItem(ItemId item, int NUMBER); //완료
 	// int값 입력하면 ItemId값으로 변환하는 함수
-	ItemId convert(int Num)
+	ItemId convert(int Num) // 완료
 	{
 		return static_cast<ItemId>(Num);
 	}
@@ -183,21 +227,13 @@ public:	//아이템 사용함수
 	// 각 ItemId에 대응하는 효과 함수를 저장할 맵핑 테이블
 	/*std::unordered_map<ItemId, std::function<void()>> effects;
 	std::unordered_map<ItemId, std::function<void()>> removeEffects;*/
-	vector<ItemId> equippedItems; // 장비한 장비 목록
+	//vector<ItemId> equippedItems; // 장비한 장비 목록
 	// TODO: 효과 중복가능 문제 미해결
 	// 장착아이템 확인(view)
-	bool ViewEquippedItems();
+	bool ViewEquippedItems(); // 완료
 	// 장착아이템 해체
-	void MoveEquipped(int choice);
-	// 아이템 효과 초기화 부
-	void initEffects();
-	void ItemTypeEffect(ItemId itemId);    // 소비아이템 사용, 장비 장착
-	void UnItemTypeEffect(ItemId itemId);  // 장비 해제
-	void applyEquippedItems();        // 장비 효과 적용
-	// ItemId를 인수로 아이템,장비 효과 함수적용, Private 처리예정
-	void applyEffect(ItemId test);
-	// ItemId를 인수로 장비 효과제거 함수적용, Private 처리예정
-	void RemoveEffect(ItemId test);
+	void MoveEquipped(ItemId id); // 완료
+	
 	
  
 
